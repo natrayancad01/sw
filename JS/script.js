3220a85c5ac26e40ae1b858fe720043a92db6b0e
natrayancad01 <natrayancad01@gmail.com>
2023-12-30 15:37:02 +0530

    add files natrayan

---
+++ JS/script.js
@@ -0,0 +1,40 @@
+function addition(){
+    
+var a=50;
+var b=37;
+var c=a+b;
+
+
+console.log("added value a and b is ... "+c);
+console.log(a*b);
+console. log(a-b);
+console.log(a/b);
+console.log(a%b);
+console.log(a+-b);
+console.log(a**b);
+console.log(a+=b);
+console.log(b+=a);
+}
+
+
+function subtraction(){
+    
+    var a=13;
+    var b=12;
+    var c=c-b;
+    
+    if(a<10)
+    {
+        console.log("a lessthan b ... ");    
+    }
+    console.log("subtracted value a and b is ... "+c);
+    }
+
+    
+addition();
+subtraction();
+//document.getElementById("menu_home").innerHTML=a;
+
+document.getElementById("firstname").innerHTML=document.getElementById("fname").value;
+document.getElementById("lastname").innerHTML=document.getElementById("lname").value;
+document.getElementById("phonename").innerHTML=document.getElementById("phnumber").value;
